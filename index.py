0c3eda1a0b073d7556eb0ebd2159e95fe829d0a4
史斌 <assasin0308@sina.com>
2020-04-12 22:24:38 +0800

    templates

--- index.py
+++ index.py
@@ -1,10 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return '响应的一句话'
 
+# templates 位置: 默认情况下,flask会在程序文件夹中的 templates 子文件夹中寻找模板
 
 
 if __name__ == '__main__':
