a02e60be5062ab67451bcb2d3b5d50717c246128
史斌 <assasin0308@sina.com>
2020-04-12 21:27:52 +0800

    url反向解析

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-from flask import Flask
-
+from flask import Flask, url_for
 
 # 将当前运行得到主程序构建成为Flask的应用,以便接受用户的请求(request)并给出相应(response)
 app = Flask(__name__)
@@ -33,6 +32,31 @@ def index_page(page=None):
     if page is None:
         page = 1
     return "<h1> 这是首页 ......,page is %d </h1>" %page
+
+# 路由中设置HTTP请求方法  POST
+@app.route('/user_list',methods=['POST']) #  ['GET','POST']
+def user_list():
+    return 'this is post request method'
+
+# URL的反向解析:通过视图处理函数的名称自动生成视图处理函数的访问路径
+# url_for() 用于反向解析URL
+# 第一个参数: 指向函数名称(通过@app.route()修饰的函数)
+# 后续参数,对应要构建的url上的变量
+
+@app.route('/url')
+def url_views():
+    # 将 url_views 反向解析访问地址
+    return url_for('index_page') # /index_page
+
+@app.route('/url2')
+def url_show():
+    return url_for('show',name='shibin',age=52) # /show/shibin/52
+
+# url_for('static',filename='style.css') # 用于静态文件反向解析
+
+# template 模板
+
+
 
 
 if __name__ == '__main__':
