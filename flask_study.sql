53b073a41d100d0400fd683d99f8b5c3669904a6
史斌 <assasin0308@sina.com>
2020-04-19 23:26:08 +0800

    关系映射/一对一&一对多

--- flask_study.sql
+++ flask_study.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50553
 File Encoding         : 65001
 
-Date: 2020-04-19 21:49:54
+Date: 2020-04-19 23:24:44
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,13 +23,13 @@ CREATE TABLE `course` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `cname` varchar(30) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of course
 -- ----------------------------
-INSERT INTO `course` VALUES ('1', 'python基础');
-INSERT INTO `course` VALUES ('2', 'python高级');
+INSERT INTO `course` VALUES ('1', 'python 基础');
+INSERT INTO `course` VALUES ('2', 'python 高级');
 INSERT INTO `course` VALUES ('3', 'python web基础');
 INSERT INTO `course` VALUES ('4', 'python web高级');
 
@@ -59,18 +59,13 @@ CREATE TABLE `teacher` (
   `course_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `course_id` (`course_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of teacher
 -- ----------------------------
-INSERT INTO `teacher` VALUES ('1', '魏老师', '35', '1');
-INSERT INTO `teacher` VALUES ('2', '王老师', '80', '3');
-INSERT INTO `teacher` VALUES ('3', '史斌', '38', '1');
-INSERT INTO `teacher` VALUES ('4', '吕老师', '20', '1');
-INSERT INTO `teacher` VALUES ('5', '马老师', '24', '3');
-INSERT INTO `teacher` VALUES ('6', '王小姐', '23', '4');
-INSERT INTO `teacher` VALUES ('7', '萌萌小姐', '18', '3');
+INSERT INTO `teacher` VALUES ('1', 'shibin', '23', '1');
+INSERT INTO `teacher` VALUES ('2', '王老师', '34', '2');
 
 -- ----------------------------
 -- Table structure for users
@@ -89,3 +84,22 @@ CREATE TABLE `users` (
 -- ----------------------------
 -- Records of users
 -- ----------------------------
+
+-- ----------------------------
+-- Table structure for wife
+-- ----------------------------
+DROP TABLE IF EXISTS `wife`;
+CREATE TABLE `wife` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `wname` varchar(30) DEFAULT NULL,
+  `wage` int(11) DEFAULT NULL,
+  `teacher_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `teacher_id` (`teacher_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of wife
+-- ----------------------------
+INSERT INTO `wife` VALUES ('5', '范冰冰', '34', '1');
+INSERT INTO `wife` VALUES ('4', '王夫人', '25', '2');
--- relation.py
+++ relation.py
@@ -50,6 +50,11 @@ class Teacher(db.Model):
     # 一门课程可以由多名老师教授  一对多 增加一列 course_id
     course_id = db.Column(db.Integer,db.ForeignKey('course.id'))
 
+    # 一名老师 只有一个 wife
+    # 增加反向引用, Teacher 中得到一个 Wife的信息,同时在Wife中也能得到一个Teacher的信息
+    wife = db.relationship('Wife',backref='teacher',uselist=False)
+    # uselist=False 查询出的是一个对象,而不是列表
+
 
     def __init__(self, tname, tage):
         self.tname = tname
@@ -57,6 +62,22 @@ class Teacher(db.Model):
 
     def __repr__(self):
         return '<Teacher:%r>' % self.tname
+
+#
+class Wife(db.Model):
+    __tablename__ = 'wife'
+    id = db.Column(db.Integer,primary_key=True)
+    wname = db.Column(db.String(30))
+    wage = db.Column(db.Integer)
+    # 增加一列 表示引用自 teacher表的主键
+    teacher_id = db.Column(db.Integer,db.ForeignKey('teacher.id'))
+
+    def __init__(self,wname,wage):
+        self.wname = wname
+        self.wage = wage
+
+    def __repr__(self):
+        return "<Wife %r>" % self.wname
 
 
 class Course(db.Model):
@@ -76,7 +97,7 @@ class Course(db.Model):
 
 # 将创建好的实体类映射回数据库
 # db.drop_all()
-# db.create_all()
+db.create_all()
 
 # 关系映射 teacher-->course
 # 每名老师教一门课程  一对一
@@ -84,6 +105,17 @@ class Course(db.Model):
 # 语法实现:
 # 1. 外键列明 = db.Column(db.Integer,db.ForeignKey('主表.主键'))
 # 2. 属性名称 = db.relationship('多的实体类名',backref='属性名',lazy='dynamic')
+    # backref:在关系的另一个模型中添加反向引用
+
+    # lazy : lazy=select:首次访问时加载
+    # lazy : lazy=immediate:原对象加载后立即加载关联数据
+    # lazy : lazy=subquery: 立即加载,但使用子查询方式
+    # lazy : lazy=noload: 永不加载
+    # lazy : lazy=dynamic: 动态加载,不加载记录,但提供加载记录的查询
+
+    # uselist 若设置为False,则不使用列表,使用标量
+
+    # secondary 指定多对多关系中关联表的名称
 
 
 @app.route('/add_course')
@@ -158,6 +190,52 @@ def query_teacher_course():
         print('老师:%s;课程:%s'%(result.Teacher.tname,result.Course.cname))
     return 'Query success'
 
+@app.route('/add_wife')
+def add_wife():
+    # 查询信息
+    teacher = Teacher.query.filter_by(id = 2).first()
+    # 创建对象
+    wife = Wife('王夫人',25)
+    # 赋值
+    wife.teacher = teacher
+    #保存
+    db.session.add(wife)
+    return 'Add wife success'
+
+@app.route('/query_wife')
+def query_wife():
+    # 通过 teacher 查询 wife
+    teacher = Teacher.query.filter_by(tname='王老师').first()
+    wife = teacher.wife
+    print("%s的媳妇是%s" %(teacher.tname,wife.wname))
+    # 通过 wife 查询 teacher
+    wife = Wife.query.filter_by(wname='王夫人').first()
+    teacher = wife.teacher
+    print('%s的官人是%s' %(wife.wname,teacher.tname))
+    return 'Query wife success'
+
+@app.route('/register_wife',methods=['GET','POST'])
+def register_wife():
+    if request.method == 'GET':
+        teachers = Teacher.query.all()
+        return render_template('register_wife.html',params = locals())
+    else:
+        teacher_id = request.form.get('teacher')
+        wife = Wife.query.filter_by(teacher_id = teacher_id).first()
+        if wife :
+            errmsg = 'exist'
+            teachers = Teacher.query.all()
+            return render_template('register_wife.html', params=locals())
+        else:
+            wname = request.form.get('wname')
+            wage = request.form.get('wage')
+            teacher = Teacher.query.filter_by(id = teacher_id).first()
+            wife = Wife(wname,wage)
+            wife.teacher = teacher
+            db.session.add(wife)
+        # print(teacher_id)
+
+        return 'register wife success'
 
 
 if __name__ == '__main__':
